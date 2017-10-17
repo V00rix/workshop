67a3150224cdd1e84d5dc8b8ca4e3239a076b223
Svitovan <svitovan@outlook.com>
2017-10-17 17:01:41 +0200

    string MyTest

--- workshopIS/Models/CPartner.cs
+++ workshopIS/Models/CPartner.cs
@@ -7,6 +7,7 @@ namespace workshopIS.Models
 {
     public class CPartner : IPartner
     {
+        string MyTest;
         private int Id;
         public string Name;
         public string Surname;
