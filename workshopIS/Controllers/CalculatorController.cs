bff80b6d26af2036b58ce72ce1db7bada6abe69c
Top Kek <vladogim97@gmail.com>
2017-10-17 16:27:13 +0200

    Added Calculator controller (GET/POST) /api/calculator

---
+++ workshopIS/Controllers/CalculatorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace workshopIS.Controllers
+{
+    public class CalculatorController : ApiController
+    {
+        [HttpPost]
+        public void Post()
+        {
+            // localhost:PORT/api/calculator
+            // TODO
+        }
+
+
+        // Dummy function
+        [HttpGet]
+        public void Get()
+        {
+            
+        }
+
+
+
+    }
+}
