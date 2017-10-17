74c15b9e7bb705920269a597a218c392e10c5fef
Top Kek <vladogim97@gmail.com>
2017-10-17 16:12:10 +0200

    Added IPartner interface and CPartner class

---
+++ workshopIS/Models/CPartner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace workshopIS.Models
+{
+    public class CPartner : IPartner
+    {
+        private int Id;
+        public string Name;
+        public string Surname;
+        public string DokumentPath;
+        public DateTime ValidFrom;
+        public DateTime ValidTo;
+        public CPartner() { }
+    }
+}
