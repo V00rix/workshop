0e6864c7faa96a1e9f444fa6566e42c117a4f3b6
David <d@vid.cool>
2017-10-18 09:35:45 +0200

    DB first config

--- workshopIS/Models/CPartner.cs
+++ workshopIS/Models/CPartner.cs
@@ -8,12 +8,12 @@ namespace workshopIS.Models
     public class CPartner : IPartner
     {
         // all params - public?
-        public int Id;
-        public string Name;
-        public string Surname;
-        public string DokumentPath;
-        public DateTime ValidFrom;
-        public DateTime ValidTo;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ICO { get; set; }
+        public string DokumentPath { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
         public CPartner() { }
     }
 }
