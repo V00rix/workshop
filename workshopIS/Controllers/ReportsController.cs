7895024a52f2ac9f2c8e866a3f1a70401394b288
David <d@vid.cool>
2017-10-18 09:56:48 +0200

    DB pokr

--- workshopIS/Controllers/ReportsController.cs
+++ workshopIS/Controllers/ReportsController.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using workshopIS.Helpers;
+using workshopIS.Models;
 
 namespace workshopIS.Controllers
 {
     public class ReportsController : ApiController
     {
+        public List<CCustomer> GetMessages()
+        {
+            NHibernate.ISession session = NHibernateHelper.GetCurrentSession();
+
+            var results = session.Query<CCustomer>();
+
+            return results.ToList<CCustomer>();
+        }
     }
 }
