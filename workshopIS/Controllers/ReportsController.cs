21cf2d7913f13489b38a511f1dd48c0fefe0c0a1
Jan Trpák <jan.trpak@unicornsystems.eu>
2017-10-20 12:25:53 +0200

    Reports

--- workshopIS/Controllers/ReportsController.cs
+++ workshopIS/Controllers/ReportsController.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -24,67 +25,102 @@ namespace workshopIS.Controllers
     public class ReportsController : ApiController
     {
 
-        [System.Web.Http.Route("api/reports/Loan/Partner/pokus")]
-
-        public IHttpActionResult GetLoanInfX()
-        {
-            ISession session = NHibernateHelper.GetCurrentSession();
-            IList<CLoan> query = session.QueryOver<CLoan>() //select from LOAN
-                .JoinQueryOver(l => l.Customer) //join CUSTOMER
-                .JoinQueryOver(l => l.Partner) //join PARTNER
-                .List();
-
-
-            return Ok(query);
-        }
-
-
+        /// <summary>
+        /// return all loans by partner grouped by state
+        /// </summary>
+        /// <returns>return all loans by partner grouped by state </returns>
         [System.Web.Http.Route("api/reports/Loan/Partner")]
-
+        
         public IHttpActionResult GetLoanInfo()
         {
             ISession session = NHibernateHelper.GetCurrentSession();
+            try
+            {
+                IList<CLoan> test = session.QueryOver<CLoan>()
+                    .JoinQueryOver(l => l.Customer)
+                    .JoinQueryOver(l => l.Partner).List();
 
+                var loanByPartner = test.GroupBy(t => t.Customer.Partner)
+                    .Select(t => new
+                    {
+                        Partner = t.Key.Id,
+                        LoansByState = t.GroupBy(x => x.Customer.ContactState)
+                            .Select(x => new {ContactState = x.Key, Loans = x.ToList()})
+                    });
 
-
-
-            IList<CLoan> test = session.QueryOver<CLoan>()
-                                    .JoinQueryOver(l => l.Customer)
-                                    .JoinQueryOver(l => l.Partner).List();
-
-            var loanByPartner = test.GroupBy(t => t.Customer.Partner)
-                .Select(t => new {Partner = t.Key.Id, LoansByState = t.GroupBy(x=>x.Customer.ContactState).Select(x=>new {ContactState=x.Key,Loans=x.ToList()})});
-
-            //GroupBy(y=>y.loan.Select(x=>x.Customer.ContactState)).Select(x=>new {customerx=x.Key});
-
-
-            return Ok(loanByPartner);
+                return Ok(loanByPartner);
+            }
+            catch
+            {
+                return BadRequest("Bad request");
+            }
+            finally
+            {
+                session.Close();
+            }
 
         }
+        /// <summary>
+        /// return all loans by partner grouped by state, not works correctly
+        /// </summary>
+        /// <returns>return all loans by partner grouped by state in CSV</returns>
+        [System.Web.Http.Route("api/reports/Loan/Partner/Csv")]
 
+        public IHttpActionResult GetLoanByCsv()
+        {
+            ISession session = NHibernateHelper.GetCurrentSession();
+
+            IList<CLoan> test = session.QueryOver<CLoan>()
+                .JoinQueryOver(l => l.Customer)
+                .JoinQueryOver(l => l.Partner).List();
+
+            var loanByPartner = test.GroupBy(t => t.Customer.Partner)
+                .Select(t => new { Partner = t.Key.Id, LoansByState = t.GroupBy(x => x.Customer.ContactState).Select(x => new { ContactState = x.Key, Loans = x.ToList() }) });
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            string json = js.Serialize(loanByPartner);
+            JsonToCsv.jsonStringToCSV(json);
+
+            return Ok();
+
+        }
+        /// <summary>
+        /// return loans by createdate
+        /// </summary>
+        /// <param name="dateFrom">dateFrom for filter</param>
+        /// <param name="dateTo">dateTo for filter</param>
+        /// <returns></returns>
         [System.Web.Http.Route("api/reports/Loan")]
 
         public IHttpActionResult GetLoanInfoByDate(string dateFrom,string dateTo)
         {
-            DateTime DateFrom=DateTime.Parse(dateFrom);
-            DateTime DateTo = DateTime.Parse(dateTo);
             ISession session = NHibernateHelper.GetCurrentSession();
-            //var test = session.QueryOver<CLoan>().JoinQueryOver(x=>x.Customer).IsBetween(X).List();
-            IList<CLoan> test = session.QueryOver<CLoan>()
-                .JoinQueryOver(l => l.Customer)
-                .JoinQueryOver(l => l.Partner).List();
-            var query=test.Where(x=>x.Customer.CreationDate>= DateFrom && x.Customer.CreationDate <= DateTo);
-            //dopsat
+            try
+            {
+                DateTime DateFrom = DateTime.Parse(dateFrom);
+                DateTime DateTo = DateTime.Parse(dateTo);
 
 
-
-
-
-
-            return Ok(query);
+                IList<CLoan> test = session.QueryOver<CLoan>()
+                    .JoinQueryOver(l => l.Customer)
+                    .JoinQueryOver(l => l.Partner).List();
+                var query = test.Where(x => x.Customer.CreationDate >= DateFrom && x.Customer.CreationDate <= DateTo);
+                return Ok(query);
+            }
+            catch
+            {
+                return BadRequest("bad request");
+            }
+            finally
+            {
+                session.Close();
+            }
         }
 
-
+        /// <summary>
+        /// Count loans by statuses in callcentre
+        /// </summary>
+        /// <returns>return counter loans by statuses</returns>
         [System.Web.Http.Route("api/reports/CallCentre/status")]
 
         public IHttpActionResult GetCallCentrumInfo()
@@ -94,52 +130,48 @@ namespace workshopIS.Controllers
             return Ok(result);
 
         }
+        /// <summary>
+        /// Count loans by unique status in callcentre
+        /// </summary>
+        /// <param name="status"></param>
+        /// <returns></returns>
         [System.Web.Http.Route("api/reports/CallCentre/status/{status}")]
 
         public IHttpActionResult GetCallCentrumInfoById(int status)
         {
-            if (status == 1 || status == 2 || status == 3)
-            {
-                ISession session = NHibernateHelper.GetCurrentSession();
-                var result = session.Query<CCustomer>().GroupBy(x => x.ContactState).Select(x => new { Status = x.Key, counter = x.Count() }).Where(x => x.Status.Value == status).ToList();
-                if (result.Count == 0)
-                {
-                    return Ok("Loans with this status was not found");
-                }
-                return Ok(result);
-
-            }
-            return BadRequest("Status doesnt exist");
-
-        }
-
-
-
-        [System.Web.Http.Route("api/reports/Partners")]
-
-        public IHttpActionResult GetPartnerInfo()
-        {
             ISession session = NHibernateHelper.GetCurrentSession();
             try
             {
+                if (status == 1 || status == 2 || status == 3)
+                {
+                    var result = session.Query<CCustomer>().GroupBy(x => x.ContactState)
+                        .Select(x => new {Status = x.Key, counter = x.Count()}).Where(x => x.Status.Value == status)
+                        .ToList();
+                    if (result.Count == 0)
+                    {
+                        return Ok("Loans with this status was not found");
+                    }
+                    return Ok(result);
 
-                var result = session.Query<CPartner>().Select(x => new { Partners = x });
-
-
-
-                return Ok(result);
+                }
+                return BadRequest("Status doesnt exist");
             }
-
-                catch (Exception e)
+            catch
             {
-                return BadRequest(e.Message);
+                return BadRequest("Bad request");
             }
             finally
             {
                 session.Close();
             }
+
+
         }
 
+        /// <summary>
+        /// count all partners
+        /// </summary>
+        /// <returns>return counted partners</returns>
         [System.Web.Http.Route("api/reports/Partners/Count")]
 
         public IHttpActionResult GetPartnerCount()
@@ -162,30 +194,12 @@ namespace workshopIS.Controllers
             {
                 session.Close();
             }
-
         }
 
-        [System.Web.Http.Route("api/reports/Loan")]
-
-        public IHttpActionResult GetAllLoanInfo()
-        {
-            ISession session = NHibernateHelper.GetCurrentSession();
-            try
-            {
-                var result = session.Query<CLoan>().Select(x => new { Loan = x });
-
-                return Ok(result);
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-            finally
-            {
-                session.Close();
-            }
-        }
-
+        /// <summary>
+        /// count all loans
+        /// </summary>
+        /// <returns>counted loans</returns>
         [System.Web.Http.Route("api/reports/Loan/Count")]
 
         public IHttpActionResult GetLoanCount()
@@ -209,7 +223,11 @@ namespace workshopIS.Controllers
             }
 
         }
-
+        /// <summary>
+        /// Count all loans by partner
+        /// </summary>
+        /// <param name="partnerId">partnerID</param>
+        /// <returns>counted loans by partnerID</returns>
         [System.Web.Http.Route("api/reports/Loan/Partner/{partnerId:int}")]
 
         public IHttpActionResult GetLoanByPartnerInfo(int partnerId)
---
+++ workshopIS/Helpers/JsonToCsv.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace workshopIS.Helpers
+{
+    public class JsonToCsv
+    {
+        public static void jsonStringToCSV(string jsonContent)
+        {
+            //used NewtonSoft json nuget package
+            XmlNode xml = JsonConvert.DeserializeXmlNode("{records:{record:" + jsonContent + "}}");
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.LoadXml(xml.InnerXml);
+            XmlReader xmlReader = new XmlNodeReader(xml);
+            DataSet dataSet = new DataSet();
+            dataSet.ReadXml(xmlReader);
+            var dataTable = dataSet.Tables[0];
+
+            //Datatable to CSV
+            var lines = new List<string>();
+            string[] columnNames = dataTable.Columns.Cast<DataColumn>().
+                Select(column => column.ColumnName).
+                ToArray();
+            var header = string.Join(",", columnNames);
+            lines.Add(header);
+            var valueLines = dataTable.AsEnumerable()
+                .Select(row => string.Join(",", row.ItemArray));
+            lines.AddRange(valueLines);
+            File.WriteAllLines(@"C:\Hatchery\Export.csv", lines);
+        }
+    }
+}
